6508aa57a65abccc73a5cea60949f57655706a5d
brunoozzetti <bruno.ozzetti94@gmail.com>
2026-04-15 16:32:44 -0300

    Update index.ts

--- api/integracoes/whatsapp/index.ts
+++ api/integracoes/whatsapp/index.ts
@@ -1,7 +1,3 @@
-TWILIO_ACCOUNT_SID=AC3b2a0eb68b4fcaf0591794c1c018525e
-TWILIO_AUTH_TOKEN=e20604458ceb7a84c77aff67ebefbd36
-TWILIO_PHONE_NUMBER=+13204334771
-
 import twilio from 'twilio';
 
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
